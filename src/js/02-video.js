90f710d91baaa504caf2859a7dd06e31c22e3998
Michalinius <michalinius@gmail.com>
2022-08-17 18:08:13 +0200

    second task completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,25 @@
-const iframe = document.querySelector("iframe");
-const player = new Vimeo.Player(iframe);
+import Vimeo from "@vimeo/player"
+import throttle from 'lodash.throttle';
 
-player.on(timeupdate, () => localStorage.setItem("videoplayer-current-time",timeupdate.seconds));
-console.log(localStorage.getItem("videoplayer-current-time"));
+const iframe = document.querySelector(`iframe`);
+const player = new Vimeo(iframe);
+
+player.on("timeupdate", throttle(({seconds}) => {
+    localStorage.setItem("videoplayer-current-time", seconds);
+    const item = localStorage.getItem("videoplayer-current-time");
+    console.log(item);
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
