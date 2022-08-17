3fd89a8e4a8a126f4c2a624eae82655ec81beab3
Michalinius <101971811+Michalinius@users.noreply.github.com>
2022-08-17 16:10:12 +0000

    Deploying to gh-pages from @ Michalinius/goit-js-hw-08@90f710d91baaa504caf2859a7dd06e31c22e3998 🚀

--- 02-video.bc3c5bcf.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe");new Vimeo.Player(e).on(timeupdate,(function(){return localStorage.setItem("videoplayer-current-time",timeupdate.seconds)})),console.log(localStorage.getItem("videoplayer-current-time"))}();
-//# sourceMappingURL=02-video.bc3c5bcf.js.map
--- 02-video.d8cfc52c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe");new Vimeo.Player(e).on(timeupdate,(()=>localStorage.setItem("videoplayer-current-time",timeupdate.seconds))),console.log(localStorage.getItem("videoplayer-current-time"));
-//# sourceMappingURL=02-video.d8cfc52c.js.map
