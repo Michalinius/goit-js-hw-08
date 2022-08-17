5effa7b7df78bd23b2e7e537df4bc592e11f5e68
Michalinius <michalinius@gmail.com>
2022-08-17 16:15:19 +0200

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 const iframe = document.querySelector("iframe");
 const player = new Vimeo.Player(iframe);
 
-
+player.on(timeupdate, () => localStorage.setItem("videoplayer-current-time",timeupdate.seconds));
+console.log(localStorage.getItem("videoplayer-current-time"));
